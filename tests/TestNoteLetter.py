bf173caff927dbcdd9716f343620055af7bd8e52
charlies-world <charliew3169@gmail.com>
2021-03-28 13:18:17 -0400

    NoteLetter how has a testing class.

---
+++ tests/TestNoteLetter.py
@@ -0,0 +1,7 @@
+import sys
+sys.path.append("C:\\Users\\ewatts3\\source\\repos\\ComPoYse\\compoyse")
+import unittest
+from setuptools import find_packages
+import pretty_midi
+
+class TestNoteLetter(unittest.case):
