34ffd9944bf948d3bb0e7788a205410e20465876
charlies-world <charliew3169@gmail.com>
2021-03-28 15:03:30 -0400

    Project will have have an AudioFile class and a test class for it.

---
+++ compoyse/AudioFile.py
@@ -0,0 +1,3 @@
+import pyaudio
+
+class AudioFile:
---
+++ tests/TestAudioFile.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.append("C:\\Users\\ewatts3\\source\\repos\\ComPoYse\\compoyse")
+import unittest
+from setuptools import find_packages
+
+class TestNote(unittest.TestCase):
