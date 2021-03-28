a064c99e22f9e907d66e1498d5fa7b56e54367dd
charlies-world <charliew3169@gmail.com>
2021-03-28 13:23:47 -0400

    NoteLetter will now have a test for its internal method find_midi_value().

--- tests/TestNoteLetter.py
+++ tests/TestNoteLetter.py
@@ -2,6 +2,11 @@ import sys
 sys.path.append("C:\\Users\\ewatts3\\source\\repos\\ComPoYse\\compoyse")
 import unittest
 from setuptools import find_packages
-import pretty_midi
+from NoteLetter import NoteLetter
 
-class TestNoteLetter(unittest.case):
+class TestNoteLetter(unittest.TestCase):
+    def test_find_midi_value__midi_value_is_100(self):
+        test_nl = NoteLetter('C#', -2)
+        test_mv = test_nl.find_midi_value('E', 7)
+        self.assertEquals(test_mv, 100, "MIDI value is 100.")
+        return
