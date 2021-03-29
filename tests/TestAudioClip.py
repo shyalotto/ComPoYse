5a18492040de2210960b2d8d14bf0a1bbb5d0b91
charlies-world <charliew3169@gmail.com>
2021-03-29 19:49:16 -0400

    AudioClip will now have tests.

---
+++ tests/TestAudioClip.py
@@ -0,0 +1,36 @@
+import sys
+sys.path.append("C:\\Users\\ewatts3\\source\\repos\\ComPoYse\\compoyse")
+import unittest
+import os
+from setuptools import find_packages
+from AudioClip import AudioClip
+from AudioFile import AudioFile
+
+class TestAudioClip(unittest.TestCase):
+    def test_set_audio_file__audio_file_is_set(self):
+        test_ac = AudioClip()
+        test_af = AudioFile()
+        test_ac.set_audio_file(test_af)
+        self.assertEquals(test_ac.get_audio_file(), test_af, "Audio file is test_af.")
+        return
+    
+    def test_set_start__start_is_set(self):
+        test_ac = AudioClip()
+        test_ac.set_start(10)
+        self.assertEquals(test_ac.get_start(), 10, "Start is 10.")
+        return
+    
+    def test_set_duration_of_playtime__duration_of_playtime_is_set(self):
+        test_ac = AudioClip()
+        test_ac.set_duration_of_playtime(10)
+        self.assertEquals(test_ac.get_duration_of_playtime(), 10, "Duration of playtime is 10.")
+        return
+    
+    def test_get_file_real_path__file_real_path_is_got(self):
+        test_ac = AudioClip()
+        test_af = AudioFile()
+        test_af.set_file_name('test_file.wav')
+        test_af.set_file_directory('tests\\test_directory')
+        correct_real_path = os.path.realpath(test_af.get_file_directory() + '\\\\' + test_af.get_file_name())
+        self.assertEquals(test_ac.get_file_real_path(), correct_real_path, "Real path is correct full directory.")
+        return
