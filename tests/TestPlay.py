cc4c768246188b94a560915ee1d7079651f7205e
charlies-world <charliew3169@gmail.com>
2021-03-29 19:55:06 -0400

    Play will now have a file for tests.

---
+++ tests/TestPlay.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append("C:\\Users\\ewatts3\\source\\repos\\ComPoYse\\compoyse")
+import unittest
+from setuptools import find_packages
+import pyaudio
+import wave
+from Play import Play
+
+class TestPlay(unittest.TestCase):
