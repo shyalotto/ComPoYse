9d98b99e7e9696cc6306816ba0122a3234fbc11d
charlies-world <charliew3169@gmail.com>
2021-03-31 19:54:47 -0400

    AudioPlayers test will now correctly reflect it's renaming.

--- tests/TestAudioPlayer.py
+++ tests/TestAudioPlayer.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append("C:\\Users\\ewatts3\\source\\repos\\ComPoYse\\compoyse\\wav")
+sys.path.append("C:\\Users\\ewatts3\\source\\repos\\ComPoYse\\compoyse\\wavgit")
 import unittest
 from setuptools import find_packages
 import pyaudio
