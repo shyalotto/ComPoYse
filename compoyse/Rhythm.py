35247313e3b4e4db2eea406dacc0d54a6612c11f
charlies-world <charliew3169@gmail.com>
2021-03-31 19:37:59 -0400

    Project will now have an enumerated type of Rhythm.

---
+++ compoyse/Rhythm.py
@@ -0,0 +1,12 @@
+import enum
+
+class Rhythm(enum.Enum):
+    TWO_HUNDRED_FIFTY_SIXTH = 1
+    ONE_HUNDRED_TWENTY_EIGHTH = 2
+    SIXTY_FOURTH = 4
+    THIRTY_SECONDTH = 8
+    SIXTEENTH = 16
+    EIGHTH = 32
+    QUARTER = 64
+    HALF = 128
+    WHOLE = 256
