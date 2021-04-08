68523b9ccc2e3c64ee59a17e02146294fe024eac
charlies-world <charliew3169@gmail.com>
2021-04-07 23:38:07 -0400

    Rhythm.py will now be properly formatted as an enum.

--- compoyse/midi/Rhythm.py
+++ compoyse/midi/Rhythm.py
@@ -1,9 +1,7 @@
 import enum
 
 class Rhythm(enum.Enum):
-    def __init__(quarter_note_length):
-        
-    TWO_HUNDRED_FIFTY_SIXTH = 1
+    TWO_HUNDRED_FIFTY_SIXTH = 1 * length_of_two_hundred_fifty_sixth_note
     ONE_HUNDRED_TWENTY_EIGHTH = 2
     SIXTY_FOURTH = 4
     THIRTY_SECONDTH = 8
