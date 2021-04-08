5f811ecd0e4d96fcfc37c3185b2ef372220ffc83
charlies-world <charliew3169@gmail.com>
2021-04-07 22:16:25 -0400

    Voice will now have getters and functional methods.

---
+++ compoyse/midi/Voice.py
@@ -0,0 +1,34 @@
+import sys
+sys.path.append("C:\\Users\\ewatts3\\source\\repos\\ComPoYse\\compoyse\\midi")
+import pretty_midi
+from Note import Note
+
+class Voice:
+    def __init__(self):
+        self.notes = []
+        return
+
+    def get_length(self):
+        length = 0
+        for i in range(0, len(self.notes)):
+            length = length + self.notes[i].get_length()
+        return length
+
+    def get_note_at_index(self, index):
+        return self.notes[index]
+
+    def add_note(self, note):
+        self.notes.append(note)
+        return
+
+    def get_midi_data(self):
+        current_place_in_time = 0
+        midi_instrument = pretty_midi.Instrument(program=pretty_midi.instrument_name_to_program('Cello'))
+        for i in range(0, len(self.notes)):
+            current_note = self.get_note_at_index(i)
+            current_note.set_start(current_place_in_time)
+            midi_instrument.notes.append(current_note.get_midi_data())
+            current_place_in_time = current_place_in_time + current_note.get_length()
+            print('CPIT: ' + str(current_place_in_time))
+            print('Current note start: ' + str(current_note.get_start()))
+        return midi_instrument
