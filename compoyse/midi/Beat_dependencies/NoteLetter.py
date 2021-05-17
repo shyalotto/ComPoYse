daac047b1153b0003a70e4955857a80fcab08e3b
charlies-world <charliew3169@gmail.com>
2021-05-16 20:37:15 -0400

    Made MIDI note validation more effecient in NoteLetter.

--- compoyse/midi/Beat_dependencies/NoteLetter.py
+++ compoyse/midi/Beat_dependencies/NoteLetter.py
@@ -3,11 +3,9 @@ from compoyse.midi.MIDIExceptions import ValueNotValidMIDIValue
 
 class _NoteLetter:
     def __init__(self, letter, octave):
-        new_midi_value = self._find_midi_value(letter, octave)
-        if(self._new_midi_value_is_in_midi_value_range(new_midi_value)):
+        if(self._new_midi_value_is_in_midi_value_range(letter, octave)):
             self.letter = letter
             self.octave = octave
-            self._set_midi_value(new_midi_value)
         return
     
     def _get_letter(self):
@@ -20,37 +18,33 @@ class _NoteLetter:
         return self.midi_value
     
     def _set_letter(self, letter):
-        new_midi_value = self._find_midi_value(letter, self.octave)
-        if(self._new_midi_value_is_in_midi_value_range(new_midi_value)):
+        if(self._new_midi_value_is_in_midi_value_range(letter, self.octave)):
             self.letter = letter
-            self._set_midi_value(new_midi_value)
         return
     
     def _set_octave(self, octave):
-        new_midi_value = self._find_midi_value(self.letter, octave)
-        if(self._new_midi_value_is_in_midi_value_range(new_midi_value)):
+        if(self._new_midi_value_is_in_midi_value_range(self.letter, octave)):
             self.octave = octave
-            self._set_midi_value(new_midi_value)
         return
         
     def _alter_octave(self, amount_to_alter_by):
         new_octave = self.octave + amount_to_alter_by
-        new_midi_value = self._find_midi_value(self.letter, new_octave)
-        if(self._new_midi_value_is_in_midi_value_range(new_midi_value)):
+        if(self._new_midi_value_is_in_midi_value_range(self.letter, new_octave)):
             self.octave = new_octave
-            self._set_midi_value(new_midi_value)
         return
         
     def _set_midi_value(self, midi_value):
         self.midi_value = midi_value
         return
     
-    def _find_midi_value(self, letter, octave):
-        return pretty_midi.note_name_to_number(letter + str(octave))
-    
-    def _new_midi_value_is_in_midi_value_range(self, value):
-        if(value >= 0 and value <= 127):
+    def _new_midi_value_is_in_midi_value_range(self, letter, octave):
+        new_value = self._find_midi_value(letter, octave)
+        if(new_value >= 0 and new_value <= 127):
+            self._set_midi_value(new_value)
             return True
         else:
-            raise ValueNotValidMIDIValue(str(value) +
+            raise ValueNotValidMIDIValue(str(new_value) +
                                          " is not a valid value for a note (must be greater than or equal to 0 or lesser than or equal to 127).")
+            
+    def _find_midi_value(self, letter, octave):
+        return pretty_midi.note_name_to_number(letter + str(octave))
