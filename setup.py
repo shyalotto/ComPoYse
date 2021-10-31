e098077c8f6e9c20984aecce0d7a86ae49de03e8
charlies-world <charliew3169@gmail.com>
2021-10-31 15:30:00 -0400

    Updated setup.py and added Manifest file

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
 from setuptools import setup
 from setuptools import find_packages
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.MD").read_text()
 
 setup(
-    name='compoyse',
-    version='0.0.1',
+    name="compoyse",
+    version="1.0.0",
+    description="Generate music compositions",
+    long_description_content_type="text/markdown",
+    long_description=README,
+    url="https://github.com/charlies-world/ComPoYse",
+    author="Charlie's World",
+    author_email="charliesdotworld@gmail.com",
+    license="MIT",
     packages = find_packages(include = ['compoyse', 'compoyse.*']),
+    include_package_data=True,
     install_requires=[
         'pyaudio',
         'pretty_midi',
         'pydub'
-        ]
+        ],
 )
